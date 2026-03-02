2f2e07d6ad23b282050020c94392a4d8e2473983
fabiannQM <faqm.faqm@gmail.com>
2026-03-02 15:37:55 -0700

    homework 5 correction

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+Route::get('/', [ProductController::class, 'index']);
 Route::resource('products', ProductController::class);
